336088dd6d7fb3acf701939468dec827344cb43f
Aakash Dhar <aakashdhar@ymail.com>
2016-11-28 20:00:25 +0530

    Revert "homestay"
    This reverts commit 48572f9d98295deacf08cb8969d670ce93c4bdc3.

--- admin/includes/footer.php
+++
@@ -1,8 +0,0 @@
-      </div>
-    </div>
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-</body>
-</html>
